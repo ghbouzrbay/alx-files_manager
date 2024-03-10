8a6d9bbc5ed08db38ff5547cc57184da806a3cd0
ghbouzrbay <atighmarcelline@gmail.com>
2024-03-10 19:25:38 +0100

    Add RedisClient class for interacting with Redis

---
+++ utils/redis.js
@@ -0,0 +1,59 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isConnected = false;
+
+    this.client.on('error', (err) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+    });
+
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClien();
+export { redisClient };
