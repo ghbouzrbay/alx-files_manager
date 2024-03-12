0c9c1a8e262e142aa5492d9e11101cc4782c6cf8
Adam <adamelmadani0129@gmail.com>
2024-03-12 11:21:21 -0700

    2.task

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import AppController from '../controllers/AppController';
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
